aba8ca17a90ce599f901f4940bd421bedd052f0f
den9995 <den9995@gmail.com>
2023-03-06 21:06:15 +0300

    сделал faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,33 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <ctype.h>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool isWord = 0, isWrong = 0;
+    int i = 0, count = 0;
+    while(str[i] != 0){
+        char curr=str[i];
+        if(isspace(curr)){
+            if(isWord && !isWrong){
+                count++;
+            }
+            isWord = 0;
+            isWrong = 0;
+            i++;
+        }
+        if(!isspace(curr)){
+            isWord = 1;
+            if(isdigit(curr)){
+                isWrong = 1;
+            }
+            i++;
+        }
+
+    }
+    if(isWord && !isWrong){
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
